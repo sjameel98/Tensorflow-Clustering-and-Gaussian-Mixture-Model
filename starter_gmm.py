16e3f603ad32545ba1bf682dd371a4b0a8b70605
arifmoh2 <43102928+arifmoh2@users.noreply.github.com>
2019-03-21 21:08:28 -0400

    Friday Work

--- starter_gmm.py
+++ starter_gmm.py
@@ -3,19 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
 
-# Loading data
-#data = np.load('data100D.npy')
-data = np.load('data2D.npy')
-[num_pts, dim] = np.shape(data)
-
-# For Validation set
-if is_valid:
-  valid_batch = int(num_pts / 3.0)
-  np.random.seed(45689)
-  rnd_idx = np.arange(num_pts)
-  np.random.shuffle(rnd_idx)
-  val_data = data[rnd_idx[:valid_batch]]
-  data = data[rnd_idx[valid_batch:]]
 
 # Distance function for GMM
 def distanceFunc(X, MU):
@@ -25,6 +12,13 @@ def distanceFunc(X, MU):
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
     # TODO
+    x2 = tf.reduce_sum(tf.square(X), 1)
+    y2 = tf.reduce_sum(tf.square(MU), 1)
+    x2 = tf.reshape(x2, [-1, 1])
+    y2 = tf.reshape(y2, [1, -1])
+    diff = x2 - 2 * X @ tf.transpose(MU) + y2
+    return diff
+
 
 def log_GaussPDF(X, mu, sigma):
     # Inputs
@@ -37,6 +31,66 @@ def log_GaussPDF(X, mu, sigma):
     # log Gaussian PDF N X K
 
     # TODO
+    D = X.shape[1]
+
+    distance = distanceFunc(X, mu)
+
+    invsigma = tf.math.reciprocal(sigma)
+
+    invsigma = -1/2 * tf.square(invsigma)
+
+    mat = tf.transpose(invsigma) *  distance
+
+    logsigma = -2 * tf.log(sigma)
+
+    coeff = logsigma - tf.log(2*tf.cast(np.pi, dtype=tf.float64))
+
+    mat = mat + tf.transpose(coeff)
+
+    return mat
+    '''
+
+def log_GaussPDF(X, mu, sigma):
+    # Inputs
+    # X: N X D
+    # mu: K X D
+    # sigma: K X 1
+    # log_pi: K X 1
+    # D: N X K
+
+    # Outputs:
+    # log Gaussian PDF N X K
+
+    # 2.1.1 Log PDF for cluster k
+
+    # compute pairwise squared distance as in part 1.1
+
+    na = tf.reduce_sum(tf.square(X), 1)
+    nb = tf.reduce_sum(tf.square(mu), 1)
+
+    # na is N x K, nb is N x K
+    # broadcast na along columns, broadcast nb along rows
+    na = tf.reshape(na, [-1, 1])
+    nb = tf.reshape(nb, [1, -1])
+
+    # return pairwise euclidean difference matrix
+    D = na - 2 * tf.matmul(X, mu, False, True) + nb  # N x K
+
+    sigma2_mat = tf.ones([tf.shape(X)[0], 1], dtype=tf.float64) * tf.reshape(tf.square(sigma), [1,
+                                                                                                -1])  # square to get variances, then reshape into row tensor
+
+    coeff_1 = -(tf.shape(X)[0] / 2) * tf.log(2 * tf.cast(np.pi, dtype=tf.float64))  # 1 X 1
+
+    coeff_2 = -(1 / 2) * tf.log(
+        tf.pow(sigma, tf.cast(tf.fill([tf.shape(sigma)[0], 1], tf.shape(X)[1]), dtype=tf.float64)))  # K x 1
+
+    coeff = coeff_1 + coeff_2
+
+    mat = -(1 / 2) * tf.multiply(tf.math.reciprocal(sigma2_mat), D)
+
+    return tf.squeeze(coeff) + mat
+
+'''
 
 def log_posterior(log_PDF, log_pi):
     # Input
@@ -47,3 +101,118 @@ def log_posterior(log_PDF, log_pi):
     # log_post: N X K
 
     # TODO
+    coeff = tf.squeeze(log_pi) + log_PDF
+    return coeff - tf.expand_dims(hlp.reduce_logsumexp(coeff), 1)
+
+
+
+if __name__ == "__main__":
+
+    # Loading data
+    # data = np.load('data100D.npy')
+    data = np.load('data2D.npy')
+    [num_pts, dim] = np.shape(data)
+    # print("hello")
+
+    # For Validation set
+    is_valid = False
+
+    if is_valid:
+        valid_batch = int(num_pts / 3.0)
+        np.random.seed(45689)
+        rnd_idx = np.arange(num_pts)
+        np.random.shuffle(rnd_idx)
+        val_data = data[rnd_idx[:valid_batch]]
+        data = data[rnd_idx[valid_batch:]]
+
+    epochs = 600
+    lr = 0.01
+    K = 5
+
+    x = tf.placeholder(name = 'x', dtype = tf.float64, shape = (None, data.shape[1]))
+    mu = tf.get_variable(name = 'mean', dtype=tf.float64, shape=(K, data.shape[1]),
+                         initializer=tf.initializers.random_normal(seed=0))
+
+    stdvec = tf.get_variable(name = 'std', dtype=tf.float64, shape=(K, 1),
+                         initializer=tf.initializers.random_normal(seed=0))
+
+    prior = tf.get_variable(name = 'pi', dtype=tf.float64, shape=(K, 1),
+                         initializer=tf.initializers.random_normal(seed=0))
+
+    sigma = tf.exp(prior)
+    log_gauss_pdf = log_GaussPDF(x, mu, sigma)
+    log_prior = hlp.logsoftmax(prior) #To ensure that priors are normalized
+    log_post = log_posterior(log_gauss_pdf, log_prior)
+
+    #Defining loss function
+    temp = hlp.reduce_logsumexp(tf.squeeze(log_prior) + log_gauss_pdf)
+    loss = -tf.reduce_sum(temp)
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        train_loss = []
+
+        for epoch in range(epochs):
+            sess.run(optimizer, feed_dict={x: data})
+            trainingloss = sess.run(loss, feed_dict={x: data})
+            train_loss.append(trainingloss)
+
+            print("Training Loss: {}".format(trainingloss))
+
+        mu_after_training = sess.run(mu)
+        sigma_after_training = sess.run(sigma)
+        posterior_after_training = sess.run(log_post, feed_dict={x: data})
+
+        print("Final mu: {}".format(mu_after_training))
+
+        plt.plot(train_loss)
+        plt.xlabel('Epochs')
+        plt.ylabel('Training Loss')
+
+        plt.title("Training Loss, K={}".format(K))
+        plt.savefig("Loss{}Clusters".format(K))
+        plt.show()
+
+        #####CLUSTERS - CHANGE THIS
+
+        final_mu = mu_after_training
+        final_posterior = posterior_after_training
+
+
+        data_cluster_mat = np.column_stack((data, np.ones((data.shape[0], 1))))
+
+        for i, point in enumerate(data_cluster_mat):
+            probabilities = final_posterior[i]
+            point[2] = np.argmax(probabilities) + 1
+
+        unique, counts = np.unique(data_cluster_mat[:, -1], return_counts=True)
+        dict_counts = dict(zip(unique, counts))
+        print(dict_counts)
+
+        for cluster in range(1, K + 1):
+
+            try:
+                percentage = dict_counts[cluster] * 100 / data.shape[0]
+                print('The percentage of points belonging to cluster {} is: {}% '.format(cluster, percentage))
+
+            except KeyError:
+                print('Cluster {} has no points belonging to it'.format(cluster))
+
+        x_mu, y_mu = final_mu.T
+        x, y, cluster_label = data_cluster_mat.T
+        plt.scatter(x, y, c=cluster_label, label='data')
+        plt.scatter(x_mu, y_mu, cmap='r', marker='X', label='centroids', c='r')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Result of running Gaussian Mixture Algorithm with K = {}'.format(K))
+        plt.legend()
+        plt.show()
+
+
+
+
+
+
--- starter_kmeans.py
+++ starter_kmeans.py
@@ -3,28 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
 
-# Loading data
-data = np.load('data2D.npy')
-#data = np.load('data100D.npy')
-[num_pts, dim] = np.shape(data)
-
-# For Validation set
-is_valid = True
-if is_valid:
-  valid_batch = int(num_pts / 3.0)
-  np.random.seed(45689)
-  rnd_idx = np.arange(num_pts)
-  np.random.shuffle(rnd_idx)
-  val_data = data[rnd_idx[:valid_batch]]
-  data = data[rnd_idx[valid_batch:]]
-
-epochs = 500
-lr = 0.01
-K = 3
 
 # Distance function for K-means
 def distanceFunc(X, MU):
-  
     # Inputs
     # X: is an NxD matrix (N observations and D dimensions)
     # MU: is an KxD matrix (K means and D dimensions)
@@ -32,37 +13,116 @@ def distanceFunc(X, MU):
     # pair_dist: is the pairwise distance matrix (NxK)
     # TODO
     # sum((x-y)^2) = sum(x^2 - 2xy + y^2)
-  x2 = tf.reduce_sum(tf.square(X), 1)
-  y2 = tf.reduce_sum(tf.square(MU), 1)
-  x2 = tf.reshape(x2, [-1, 1])
-  y2 = tf.reshape(y2, [1, -1])
-  diff = x2 - 2 * X@tf.transpose(MU) + y2
-  return diff
-
-def kmeans(epochs, clusters, step, data):
-  D = data.shape[1]
-  N = data.shape[0]
-  K = clusters
-  
-  centers = tf.get_variable(name = 'MU', dtype = tf.float64, shape = (K, D), initializer = tf.initializers.random_normal(seed = 1))
-  inputs = tf.placeholder(name = 'inputs', dtype = tf.float64, shape = (None, D))
-  
-  training_losses = []
-  valid_losses = []
-  loss = tf.reduce_sum(tf.reduce_min(distanceFunc(inputs, centers), axis = 1))
-  optimizer = tf.train.AdamOptimizer(learning_rate = step).minimize(loss)
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for epoch in range(epochs):
-      training_loss = sess.run(loss, feed_dict = {inputs:data})
-      valid_loss = sess.run(loss, feed_dict = {inputs:data})
-      sess.run(optimizer, feed_dict = {inputs: data})
-      training_losses.append(training_loss)
-      valid_losses.append(valid_loss)
-      print('Training loss = {} | Validation loss = {}'.format(training_loss, valid_loss))
-    centroids = sess.run(centers)
-    
-    print(type(centroids))
+    x2 = tf.reduce_sum(tf.square(X), 1)
+    y2 = tf.reduce_sum(tf.square(MU), 1)
+    x2 = tf.reshape(x2, [-1, 1])
+    y2 = tf.reshape(y2, [1, -1])
+    diff = x2 - 2 * X @ tf.transpose(MU) + y2
+    return diff
 
 
-kmeans(epochs = epochs, clusters = K, step = 0.01, data = data)
+def kmeans(epochs, clusters, step, data, val_data):
+    D = data.shape[1]
+    N = data.shape[0]
+    K = clusters
+
+    #Initializing Placeholders & Variables
+    centers = tf.get_variable(name='MU', dtype=tf.float64, shape=(K, D),
+                              initializer=tf.initializers.random_normal(seed=1))
+    inputs = tf.placeholder(name='inputs', dtype=tf.float64, shape=(None, D))
+
+    training_losses = []
+    valid_losses = []
+    loss = tf.reduce_sum(tf.reduce_min(distanceFunc(inputs, centers), axis=1))
+    optimizer = tf.train.AdamOptimizer(learning_rate=step, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(epochs):
+            #Why do we feed in the same data twice?
+
+            training_loss = sess.run(loss, feed_dict={inputs: data})
+            valid_loss = sess.run(loss, feed_dict={inputs: val_data})
+            sess.run(optimizer, feed_dict={inputs: data})
+            training_losses.append(training_loss)
+            valid_losses.append(valid_loss)
+            print('Training loss = {} | Validation loss = {}'.format(training_loss, valid_loss))
+        centroids = sess.run(centers)
+
+        plt.plot(training_losses)
+        plt.xlabel("Number Of Iterations")
+        plt.ylabel("Loss")
+        plt.title("Training Loss, K={}".format(K))
+        plt.savefig("Loss{}Clusters".format(K))
+        plt.show()
+
+        #print(type(centroids))
+        #print(centroids.shape)
+        #print(data.shape)
+
+        dist = distanceFunc(data, centroids)
+        cluster_class = tf.argmin(dist, axis=1)
+        cluster_class = cluster_class.eval()
+
+        clusterID, count = np.unique(cluster_class, return_counts=True)
+        counts = dict(zip(clusterID, count))
+
+        for i in range(K):
+            p = counts[i]/N * 100
+            print("Percentage of points in cluster {}: {}%".format(i, p))
+
+
+
+
+        #print(cluster_class.shape)
+        #print(cluster_class)
+
+        datanp = data
+
+        x = datanp[:, 0]
+        y = datanp[:, 1]
+        centroids_x = centroids[:, 0]
+        centroids_y = centroids[:, 1]
+
+
+        plt.scatter(x, y, c=cluster_class, label='Data Points', s=25, alpha=0.8, cmap = 'Dark2')
+
+        mark = ['o', '+', 'h', 'd', '*', 'P']
+        for i in range(centroids_x.shape[0]):
+            plt.scatter(centroids_x[i], centroids_y[i], marker=mark[i], label='Cluster {} Mean'.format(i), s=100, c='k')
+
+
+
+        #plt.scatter(centroids_x, centroids_y, marker='x', label='Cluster Means', c='k', s=100)
+        plt.legend()
+        plt.title("K-Means_{}Clusters".format(K))
+        plt.savefig("K-Means_{}Clusters".format(K))
+        plt.show()
+
+
+
+
+if __name__ == "__main__":
+
+    # Loading data
+    data = np.load('data2D.npy')
+    #data = np.load('data100D.npy')
+    [num_pts, dim] = np.shape(data)
+
+    # For Validation set
+    is_valid = True
+    if is_valid:
+      valid_batch = int(num_pts / 3.0)
+      np.random.seed(45689)
+      rnd_idx = np.arange(num_pts)
+      np.random.shuffle(rnd_idx)
+      val_data = data[rnd_idx[:valid_batch]]
+      data = data[rnd_idx[valid_batch:]]
+
+    epochs = 600
+    lr = 0.01
+    K = 5
+
+
+
+
+    kmeans(epochs = epochs, clusters = K, step = lr, data = data, val_data = val_data)
